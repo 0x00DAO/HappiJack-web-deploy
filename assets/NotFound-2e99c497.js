2fcf9ba0c7559d19b7fa9677b050ab723210d92e
enixlee <405284695@qq.com>
2023-08-01 21:34:05 +0800

    deploy

---
+++ assets/NotFound-2e99c497.js
@@ -0,0 +1,1 @@
+import{s as e,S as t,C as n}from"./index-e4914d1a.js";function i(){return e.jsx(e.Fragment,{children:e.jsx(t,{direction:"vertical",style:{display:"grid",justifyContent:"center",alignContent:"center",height:`${n}`},children:e.jsx("label",{style:{fontSize:30,color:"#52c41a"},children:"Page Not Found."})})})}export{i as default};
